777253875fa177f93c5bed2399ff2f49b922485f
a_silivanov <andrey.silivanovv@gmail.com>
2018-01-29 16:40:08 +0200

    install laratrust

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Install Package Service Providers...
+         */
+        Laratrust\LaratrustServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +230,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+        * Install Package Service Providers...
+        */
+        'Laratrust'   => Laratrust\LaratrustFacade::class,
 
     ],
 
