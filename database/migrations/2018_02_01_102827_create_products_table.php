17b0a6eabae67012ffdce3e6ff5e5b935830111c
a_silivanov <andrey.silivanovv@gmail.com>
2018-02-01 13:53:27 +0200

    added products model

---
+++ database/migrations/2018_02_01_102827_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('category_id');
+            $table->integer('old_amount');
+            $table->integer('amount');
+            $table->integer('count');
+            $table->string('view_name');
+            $table->integer('position');
+            $table->boolean('published')->default(true);
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
