16c4a31bb610fa791df8cc410b6e4fb74a546dc1
a_silivanov <andrey.silivanovv@gmail.com>
2018-02-09 17:57:57 +0200

    file uplaoad component

--- resources/assets/admin/js/vue.js
+++ resources/assets/admin/js/vue.js
@@ -5,7 +5,8 @@ import Paginate from 'vuejs-paginate'
 Vue.component('table-vue', require('./vue/components/table'));
 Vue.component('paginate', Paginate);
 Vue.component('delete', require('./vue/components/delete'));
-Vue.component('modal', require('./vue/components/modal.vue'));
+Vue.component('modal', require('./vue/components/modal'));
+Vue.component('upload-file', require('./vue/components/upload-file'));
 
 Vue.component('products-index', require('./vue/pages/products/index.vue'));
 
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,4 +1,5 @@
 @extends('admin.layouts.app')
 @section('content')
     <h2>sdsdasd</h2>
+    <upload-file></upload-file>
 @endsection
