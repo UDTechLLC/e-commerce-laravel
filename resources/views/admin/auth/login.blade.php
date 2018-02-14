02504067d52c89a538d3eabe0ef587a561b5e706
vagrant <julja.ostrun@gmail.com>
2018-02-14 11:58:36 +0000

    edit_admin_login

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -23,13 +23,14 @@
     <div class="login_wrapper">
         <div class="animate form login_form">
             <section class="login_content">
-                <form>
+                <form method="POST">
+                    {{ csrf_field() }}
                     <h1>Login Form</h1>
                     <div>
-                        <input type="text" class="form-control" placeholder="Username" required="" />
+                        <input id="email" type="email" class="form-control" placeholder="Email" required="" />
                     </div>
                     <div>
-                        <input type="password" class="form-control" placeholder="Password" required="" />
+                        <input id="password" type="password" class="form-control" placeholder="Password" required="" />
                     </div>
                     <div>
                         <a class="btn btn-default submit" href="index.html">Log in</a>
@@ -52,16 +53,17 @@
 
         <div id="register" class="animate form registration_form">
             <section class="login_content">
-                <form>
+                <form method="POST">
+                    {{ csrf_field() }}
                     <h1>Create Account</h1>
                     <div>
                         <input type="text" class="form-control" placeholder="Username" required="" />
                     </div>
                     <div>
-                        <input type="email" class="form-control" placeholder="Email" required="" />
+                        <input id="email" type="email" class="form-control" placeholder="Email" required="" />
                     </div>
                     <div>
-                        <input type="password" class="form-control" placeholder="Password" required="" />
+                        <input id="password" type="password" class="form-control" placeholder="Password" required="" />
                     </div>
                     <div>
                         <a class="btn btn-default submit" href="index.html">Submit</a>
