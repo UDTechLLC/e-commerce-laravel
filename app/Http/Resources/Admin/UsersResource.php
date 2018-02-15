18af13853a98a46e92e5c88357e116a9a1124b35
vagrant <julja.ostrun@gmail.com>
2018-02-15 08:28:53 +0000

    usersResource

---
+++ app/Http/Resources/Admin/UsersResource.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class UsersResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+
+    }
+}
