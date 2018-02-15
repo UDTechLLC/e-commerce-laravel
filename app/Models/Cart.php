6576f21e714186312d22ff0a84c2a3794481c609
a_silivanov <andrey.silivanovv@gmail.com>
2018-02-15 17:00:01 +0200

    added model cart

---
+++ app/Models/Cart.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\{
+    Builder
+};
+use App\Models\EloquentModel;
+
+/**
+ * Class Cart
+ * @package App\Models
+ */
+class Cart extends EloquentModel
+{
+    /**
+     * @var string
+     */
+    protected $table = 'carts';
+
+    /**
+     * The attributes that are mass assignable.
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'product_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     * @var array
+     */
+    protected $casts = [];
+
+    /**
+     * The relationships that should be touched on save.
+     * @var array
+     */
+    protected $touches = [];
+
+    /**
+     * The relations to eager load on every query.
+     * @var array
+     */
+    protected $with = [];
+
+    /**
+     * The accessors to append to the model's array form.
+     * @var array
+     */
+    protected $appends = [];
+
+    /**
+     * Entity relations go below
+     */
+
+    // @todo:
+
+    /**
+     * Entity scopes go below
+     */
+
+    // @todo:
+
+    /**
+     * Entity mutators and accessors go below
+     */
+
+    // @todo:
+
+    /**
+     * Entity public methods go below
+     */
+
+    // @todo:
+}
