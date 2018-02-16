eab90de9f24059d18f2f9c2dec099841ea51250e
Michael Nikolaev <nika.free.dev@gmail.com>
2018-02-16 17:52:22 +0200

    checkout first and second page

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,6 +1,5 @@
 (function( $ ) {
     $( document ).ready( function() {
-        console.log(1);
         if ( 0 < $( '#successStories' ).length ) {
             $( '#successStories' ).slick({
                 autoplay: true,
@@ -21,6 +20,25 @@
                         }
                     }
                 ]
+            });
+        }
+        if ( 0 < $( '.customer-login-form' ).length ) {
+            $( '.showlogin' ).on( 'click', function() {
+                $( '.customer-login-form' ).slideToggle();
+                return false;
+            });
+        }
+        if ( 0 < $( '.create-account-block' ).length ) {
+            $( '#bdCreateAccount' ).on( 'change', function () {
+                $( '.create-account-block' ).slideToggle();
+            })
+        }
+        if ( 0 < $( '#bdCountry' ).length ) {
+            $( '#bdCountry' ).select2();
+        }
+        if ( ( 0 < $( '#shipToDifferentAddressCheckbox' ).length ) && ( 0 < $( '.contacts-block' ).length ) ) {
+            $( '#shipToDifferentAddressCheckbox' ).on( 'change', function () {
+                $( '.contacts-block' ).slideToggle();
             });
         }
     });
