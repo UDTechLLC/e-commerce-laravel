edbae54a9b552ca4f480b4129b843868006ac7ad
a_silivanov <andrey.silivanovv@gmail.com>
2018-02-19 09:38:51 +0200

    fixed conflict

--- resources/views/web/shop/products/index.blade.php
+++ resources/views/web/shop/products/index.blade.php
@@ -1,33 +1,7 @@
-<<<<<<< HEAD
-<!doctype html>
-
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-
-    <title>ShedYourFat Shop page</title>
-
-    <link rel="stylesheet" href="{{ asset('web/css/base.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('web/carouselengine/initcarousel-1.css') }}">
-    <!--<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('web/slick/slick.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('web/slick/slick-theme.css') }}">
-    <!--[if lt IE 9]>
-    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-<div class="main" id="app">
-=======
 @extends('web.layouts.app')
 @section('content')
     <main>
 <div class="main">
->>>>>>> 60fd362a2d97bcf967c5a988991196eab4b59835
         <div class="shop-grid-wrapper">
             <div class="wrapper">
                 <div class="shop-items-wrapper">
