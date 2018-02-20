85d25a8db23d2ed540c43258f0824896eea5ffd7
vagrant <julja.ostrun@gmail.com>
2018-02-20 09:42:10 +0000

    edit_14_day_detox

--- resources/views/web/shop/products/14-day-detox-guide.blade.php
+++ resources/views/web/shop/products/14-day-detox-guide.blade.php
@@ -118,6 +118,80 @@
             </div>
         </div>
     </section>
+
+    <section class="grey-section section__wrapper">
+        <div class="wrapper">
+            <div class="flex flex-j--between flex--m-block">
+                <div class="block-50 m-block-100">
+                    <div class="section__img section__img--detox">
+                        <img src="{{ asset('web/images/full-product/14-day-detox-guide/benefits.png')}}" alt="benefits">
+                    </div>
+                </div>
+                <div class="block-50 m-block-100">
+                    <h2 class="section__title section__title--detox">BENEFITS OF “14 DAY DETOX
+                        SMOOTHIE CLEANSE”</h2>
+                    <p class="section__text section__text--detox">
+                        There are a million and one ways to improve your health and it
+                        is easy to become overwhelmed by all of the what, where, when and why. Not anymore!
+                        <br><br>
+                        Our 14 Day Detox Smoothie Cleanse provides you with step by step instructions on an ALL NATURAL way to cleanse the body
+                        and improve the way you look and feel. </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section>
+        <div class="training-plan-faqs-block-wrapper">
+            <div class="wrapper">
+
+                <div class="flex flex-a--center flex--m-block detox-section__wrapperVideo">
+
+
+                    <div class="get-more__text">
+                        <img src="{{ asset('web/images/full-product/14-day-detox-guide/refresh-and-reset.png')}}" alt="get-more">
+                    </div>
+
+                    <div class="video-block-wrapper">
+                        <div class="video-block">
+                            <div id="amazingcarousel-container-1">
+                                <div id="amazingcarousel-1" style="display: block; position: relative; width: 100%; max-width: 990px; margin: 0px auto; direction: ltr;">
+                                    <div class="amazingcarousel-list-container" style="position: relative; margin: 0px auto; overflow: visible;">
+                                        <div class="amazingcarousel-list-wrapper" style="overflow: hidden;"><div class="amazingcarousel-list-wrapper" style="overflow: hidden;"><ul class="amazingcarousel-list" style="display: block; position: relative; list-style-type: none; list-style-image: none; background-image: none; background-color: transparent; padding: 0px; margin: 0px;">
+                                                    <li class="amazingcarousel-item amazingcarousel-item-0" style="display: block; position: relative; background-image: none; background-color: transparent; margin: 0px; padding: 0px; float: left;">
+                                                        <div class="amazingcarousel-item-container" style="position: relative; margin: 0px 4px;">
+                                                            <div class="amazingcarousel-image border-red">
+                                                                <a href="https://player.vimeo.com/video/230763222" title="Rauve workout plan GYM" class="html5lightbox" data-group="amazingcarousel-1"><img src="{{ asset('web/images/full-product/14-day-detox-guide/651403317_640.jpg')}}" alt="Rauve workout plan GYM"></a>
+                                                                <div class="amazingcarousel-play-video" style="position:absolute;left:0;top:0;width:100%;height:100%;cursor:pointer;background-image:url('/assets/carouselengine/playvideo-64-64-0.png');background-repeat:no-repeat;background-position:center center;"></div><div class="amazingcarousel-play-video" style="position:absolute;left:0;top:0;width:100%;height:100%;cursor:pointer;background-image:url('http://localhost:5000/assets/carouselengine/playvideo-64-64-0.png');background-repeat:no-repeat;background-position:center center;"></div></div>
+                                                        </div>
+                                                    </li>
+                                                    <div style="clear:both;"></div><div style="clear:both;"></div></ul></div></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+                </div>
+
+
+
+                <div class="training-plan-faqs-block">
+
+
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </section>
+
 </main>
 
 
