5dd5a804edc9b34357c0cddb0500d9f142a9964d
vagrant <julja.ostrun@gmail.com>
2018-02-23 11:42:31 +0000

    edit_old_amount

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -142,6 +142,11 @@ class Product extends EloquentModel implements HasMedia
     /**
      * Entity public methods go below
      */
+    /**
+     * @param $value
+     *
+     * @return string
+     */
     public function getOldAmountAttribute($value)
     {
         return number_format($value / 100, 2);
