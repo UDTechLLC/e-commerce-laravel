d30eaa7035daa8c6126e1e500412f68760ddfa32
vagrant <julja.ostrun@gmail.com>
2018-02-26 09:14:49 +0000

    edit_order_admin

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -39,7 +39,9 @@
             @if($orders->user_id)
             <td>{{ $orders->user->first_name }} , {{ $orders->user->email }} </td>
                @else
-            <td>Unregister user</td>
+            {{--<td>Unregister user</td>--}}
+               <td>{{ $orders->billing->first_name}} {{ $orders->billing->last_name}}, <br> {{$orders->billing->email}}</td>
+
             @endif
 
             @if($orders->shipping_id)
@@ -51,14 +53,11 @@
              <td> - </td>
             @endif
 
-            @if ($orders->billing_id)
             <td style="text-transform: capitalize">{{ $orders->billing->first_name}} {{ $orders->billing->last_name}},
                {{$orders->billing->company_name}}, {{ $orders-> billing->street }} str, {{$orders->billing->apartment}},
                {{ $orders->billing->country }}, {{ $orders->billing->city }} ,
                {{$orders->billing->state}}, {{$orders->billing->postcode}}</td>
-            @else
-               <td> - </td>
-            @endif
+
             <td>{{ $orders->created_at->format('M j, Y') }}</td>
             <td>{{ $orders->product_cost }}</td>
             <td>{{ $orders->shipping_cost }}</td>
