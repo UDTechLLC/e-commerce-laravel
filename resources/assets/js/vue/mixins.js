84803a9c9bd3ede257b27543bdb6ba8b79a2c33b
a.silivanov <andrey.silivanovv@gmail.com>
2018-03-01 13:32:18 +0200

    fixed shipping

--- resources/assets/js/vue/mixins.js
+++ resources/assets/js/vue/mixins.js
@@ -32,6 +32,7 @@ Vue.mixin({
       this.products = response.data.data.products.data;
       this.countItems = response.data.data.sum.products_counts;
       this.subTotal = response.data.data.sum.products_sum;
+      this.isShipping = response.data.data.isShipping;
     },
 
     makeCardHash() {
