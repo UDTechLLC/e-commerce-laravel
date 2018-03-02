b64e7ed0c1cd650dcaf16df37a6c5655ac4725dc
vagrant <julja.ostrun@gmail.com>
2018-03-02 12:18:50 +0000

    add_page_info

---
+++ resources/views/web/page/about_us.blade.php
@@ -0,0 +1,22 @@
+@extends('web.layouts.app')
+@section('title')
+    Nutrition Programs | Weight Loss Teas | Detox Tea For Weight Loss -@parent
+@endsection
+@section('content')
+    <main>
+        <div class="main" style="margin: 20px;">
+            <div class="wrapper">
+                <div class="page-content">
+                <p class="cr-content">At ShedFat, we are your fitness family! Our sole mission is to ensure you reach your fitness goals and make the progress you set out to make. We strive to arm you with the latest in nutritional and workout information to make sure you get to where you want to be in the most efficient way possible. We are 100% committed to you and we will be there for you every step of the way.</p>
+            <p class="cr-content">Rauve Haley:</p>
+                <p class="cr-content">Meet Coach Rauve Haley. Fitness has been his life for over a decade. Growing up in Virginia Beach, Virginia, he originally got into lifting and nutrition due to being unsatisfied with his body. He took it upon himself to create the change he wanted to see. Not long after embarking on his own fitness journey, he realized that his motivations weren’t all aesthetic. He started to become addicted to the way fitness was positively impacting his life physically, mentally, and emotionally.</p>
+                <p class="cr-content">After seeing how he could positively change his own life and body, Rauve became dedicated to helping others do the same. Rauve graduated from Virginia Commonwealth University with BS in Exercise Science and is an ACSM and HFS certified trainer. He prides himself on his extensive knowledge of biomechanics, anatomy, and nutrition. He considers himself a student of the fitness lifestyle, and is constantly learning better ways to help his clients. Soon after graduating from college, he started his own personal training company and has helped countless clients achieve their fitness goals.</p>
+                <p class="cr-content">Mikey Nieves:</p>
+                <p class="cr-content">Meet Coach Mikey Nieves. Mikey’s fitness journey started over ten years ago. While living in Florida as a teenager, most of Mikey’s friends were jacked. He decided then that he wanted to be more like them and attract more of the opposite sex. It didn’t take long for him to fall in love with fitness. He even started working at a gym in Florida and became a NASM certified trainer.</p>
+                <p class="cr-content">Only a few short years after beginning his journey, Mikey decided to take his love for fitness to the next level and decided to try out competitive bodybuilding. Taking this huge leap allowed Mikey to take his physique to the next level and unlocked progress he never thought imaginable. He quickly became a nationally ranked NPC competitor. As his physique constantly improved, people started asking him for advice and how to make the same progress in their physiques. As more and more people were coming to him, he realized that not only did he have a passion for improving himself, but he was truly passionate about helping others reach their fitness goals as well. As a father of two who vowed never to have a “dad bod”, Mikey has never made excuses for himself and has consistently pushes forward. Now he’s waiting to help YOU do the same.</p>
+                 </div>
+            </div>
+        </div>
+        <br>
+    </main>
+    @endsection
---
+++ resources/views/web/page/return_policy.blade.php
@@ -0,0 +1,31 @@
+@extends('web.layouts.app')
+@section('title')
+    Detox Tea For Weight Loss | Nutrition Programs | Weight Loss Teas -@parent
+@endsection
+@section('content')
+    <main>
+        <div class="main" style="margin: 20px;">
+            <div class="wrapper">
+                <div class="page-content">
+                    <ul style="list-style: outside;box-sizing: border-box; padding-left: 30px;">
+                        <li>By use of this website, you agree to the terms and conditions stipulated herein. ShedFat LLC reserves the right to modify these Terms and Conditions at any time.</li>
+                        <li>All products and services acquired from ShedFat LLC are for personal use only. You may not sell or resell any of the products or services you receive from us. We reserve the right to cancel any orders that may result in a violation of these Terms and Conditions.</li>
+                        <li>For your protection, we do not allow returns of supplements for any reason if seal is broken. If seal is unopened, you may ship the product back to our warehouse (at your expense) for a refund minus shipping costs. If we make an error, it will be quickly corrected at our expense.</li>
+                        <li>ShedFat LLC will not issue any refunds or returns on digital products. All sales made on digital products are FINAL.</li>
+                        <li>If a custom plan is purchased at full price, without the use of a discount code, coupon, or during a sale; the purchaser can submit a request for a refund as long as no questionnaire has been submitted and the request&nbsp;is sent to ShedFat LLC on or before the 30th day after purchase.</li>
+                        <li>Purchase of Custom Workout and Meal Plans will expire 6 months after the user has received their plan. ShedFat LLC will not issue refunds for plans that have not been used within 6 months of receipt.</li>
+                        <li>Email/accountability support must be purchased to be eligible to receive custom plan adjustments, otherwise adjustments will not be made for the duration of the plan.</li>
+                        <li>The link to your questionnaire will expire after 6 months and ShedFat LLC will not issue a refund for plan purchases wherein the questionnaire was not sent back to ShedFat LLC within the time-period stated above.</li>
+                        <li>Once the questionnaire has been submitted and a plan has been created, ShedFat LLC will not refund or create a new plan due to changing food preferences or availability to equipment.</li>
+                        <li>The turnaround time for purchased plans is only an estimate and though we aim to send out your plan as soon as possible, we cannot guarantee you will receive your plan within the given time window.</li>
+                        <li>All purchases made with a discount code, coupon, or on a sale are not eligible for exchanges or refunds.</li>
+                        <li>ShedFat does not warrant the quality, accuracy, or completeness of any claims, statements, or information on this Website. Further, ShedFat makes no representations about the suitability of any of the information and/or products shown on this Website or any other website for any purpose. Visitors who use this Website and rely on any information do so at their own risk. All products and information are provided “as is” and “as available” without warranty or condition of any kind. In no event shall ShedFat be liable for any damages whatsoever, including special, indirect, or consequential damages, arising out of or about the use of content available on this Website.</li>
+                        <li>Shedfat is NOT RESPONSIBLE FOR LOST OR STOLEN MERCHANDISE</li>
+                        <li>Shipping costs are non-refundable.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <br>
+    </main>
+    @endsection
