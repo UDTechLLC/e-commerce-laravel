ecf4a34cdd90be43784b45edb943dc6fc901e441
a.polikarpov <polik.palam@gmail.com>
2018-03-14 15:25:48 +0200

    Added logining

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Auth;
 
+use App\Transformers\Api\UserTransformer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use \App\Http\Controllers\Auth\LoginController as Login;
@@ -10,6 +11,6 @@ class LoginController extends Login
 {
     protected function authenticated(Request $request, $user)
     {
-        //
+        return fractal($user, new UserTransformer())->respond();
     }
 }
