e978eec742fbeb2633bd399bbe65b3f10a998bbd
a_silivanov <andrey.silivanovv@gmail.com>
2018-03-19 15:08:25 +0200

    added live chat

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -42,6 +42,16 @@
                    src="https://www.facebook.com/tr?id=252804818530741&ev=PageView&noscript=1"
         /></noscript>
     <!-- End Facebook Pixel Code -->
+    
+    <!--Start of Zendesk Chat Script-->
+    <script type="text/javascript">
+        window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+                d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+        _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+            $.src="https://v2.zopim.com/?2uXlJpXcB35h8f9oclSZEsjEXUtL6tNN";z.t=+new Date;$.
+                    type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+    </script>
+    <!--End of Zendesk Chat Script-->
 
 </head>
 <body>
