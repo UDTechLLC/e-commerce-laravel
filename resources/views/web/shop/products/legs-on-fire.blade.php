b3b35498121f5317d8af75c63cce9c2984de4b1c
vagrant <julja.ostrun@gmail.com>
2018-03-20 11:55:21 +0000

    detox

--- resources/views/web/shop/products/legs-on-fire.blade.php
+++ resources/views/web/shop/products/legs-on-fire.blade.php
@@ -60,7 +60,7 @@
                         <div class="custom-video-block__inner">
 
                             <div class="custom-video-block__wrapper-click">
-                                <img src="{{ asset('web/images/662939626_640.jpg')}}" alt="mainImage" class="custom-video-block__coverImg">
+                                <img src="{{ asset('web/images/new-images/cap.png')}}" alt="mainImage" class="custom-video-block__coverImg">
                                 <img src="{{ asset('web/carouselengine/playvideo-64-64-0.png')}}" alt="" class="custom-video-block__playImg">
                             </div>
 
