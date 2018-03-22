7a90130c87293fedb2cbd4157fd63292aa3dfd29
a.polikarpov <polik.palam@gmail.com>
2018-03-22 10:23:35 +0200

    Add deploy script

---
+++ bin/deploy-stagesrv-dev.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+remote=$(git remote)
+git pull "$remote" "$branch"
+
+changedFiles="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+checkRun() {
+    echo "$changedFiles" | grep --quiet "$1" && eval "$2"
+}
+
+composer dumpautoload
+checkRun "composer.json" "composer update"
+checkRun "database/migrations" "php artisan migrate"
+checkRun "package.json" "yarn"
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan clear-compiled
+php artisan optimize
+npm run dev
