54b205e09f82e023404212b17ccd0c2e9d7b4e2f
a.polikarpov <polik.palam@gmail.com>
2018-03-23 12:35:21 +0200

    Added error payment page

--- resources/views/errors/error_payment.blade.php
+++ resources/views/errors/error_payment.blade.php
@@ -4,17 +4,12 @@
         <div class="main">
             <div class="billing-address-info-block-wrapper">
                 <div class="wrapper">
-                    <div class="billing-address-info-block">
+                    <div class="billing-address-info-block" style="background: #FFE8E8; text-align: center;">
                         <div class="cart-header-block">
-                            <h2 class="cart-heading">
-                                Payment error
+                            <h2 class="cart-heading" style="font-size: 1.3em; color: #CE4747;">
+                                {{ $message }}
                             </h2>
-
                             <div class="clear"></div>
-                        </div>
-                        <div class="billing-user-info-block">
-                            <span> Error  </span>
-
                         </div>
                     </div>
                 </div>
