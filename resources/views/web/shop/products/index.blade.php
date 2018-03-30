9a8c81920bc3ae037a7062515c4f8746bea26d9f
a_silivanov <andrey.silivanovv@gmail.com>
2018-03-30 10:35:28 +0300

    fixed preorder

--- resources/views/web/shop/products/index.blade.php
+++ resources/views/web/shop/products/index.blade.php
@@ -53,7 +53,7 @@
                                     product-slug="{{ $product->slug }}"
                                     data-title="{{ $product->title }}"
                                     >
-                                        @if($product->id == 22) preorder @endif
+                                        @if($product->slug == 'shedfat-maxx') preorder @endif
                                     </add-to-cart>
                                 </div>
                             </div>
