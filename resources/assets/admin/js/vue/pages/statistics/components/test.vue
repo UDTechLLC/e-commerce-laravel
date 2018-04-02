f90b4a5b42bdb4199771c77e164d64784067f6fa
a_silivanov <andrey.silivanovv@gmail.com>
2018-04-02 17:25:50 +0300

    test stats

--- resources/assets/admin/js/vue/pages/statistics/components/test.vue
+++ resources/assets/admin/js/vue/pages/statistics/components/test.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="dashboard_graph">
+  <!--  <div class="dashboard_graph">
         <div class="col-md-6 col-sm-9 col-xs-12">
             <div class="demo-placeholder">
                 <line-chart
@@ -17,7 +17,25 @@
                 <button @click="fillData()">Randomize</button>
             </div>
         </div>
-    </div>
+    </div>-->
+
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Line graph<small>Sessions</small></h2>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <line-chart
+                            :chart-data="datacollection"
+                            :options="{responsive: true, maintainAspectRatio: false}"
+                    ></line-chart>
+                </div>
+            </div>
+        </div>
+        </div>
+
 </template>
 <script type="text/babel">
     import LineChart from './lineCharts.js'
@@ -47,15 +65,16 @@
                     labels: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(),this.getRandomInt()],
                     datasets: [{
                         label: 'Bar Dataset',
-                        data: [10, 20, 30, 40]
+                        borderColor: '#eeccbb',
+                        data: [10, 20, 30, 4, 11]
                     }, {
                         label: 'Line Dataset',
-                        data: [5, 25, 40, 10],
+                        data: [5, 25, 40, 10, 32],
 
                         // Changes this dataset to become a line
                         type: 'line'
                     }],
-                    labels: ['January', 'February', 'March', 'April']
+                   // labels: ['January', 'February', 'March', 'April']
                 }
             },
             getRandomInt () {
--- resources/assets/admin/js/vue/pages/statistics/index.vue
+++ resources/assets/admin/js/vue/pages/statistics/index.vue
@@ -1,18 +1,47 @@
 <template>
     <div>
-        <h1>Statistics</h1>
+        <h1>Analytics</h1>
+        <div class="bs-example-popovers">
+            <button type="button" class="btn btn-default active">
+                Year
+            </button>
+            <button type="button" class="btn btn-default">
+                Month
+            </button>
+            <button type="button" class="btn btn-default">
+                Week
+            </button>
+            <button type="button" class="btn btn-default">
+                Day
+            </button>
+            <span>Custom: </span>
+            <datepicker v-model="startDate"></datepicker> -
+            <datepicker v-model="endDate"></datepicker>
+        </div>
             <test></test>
     </div>
 </template>
 <script type="text/babel">
     import test from './components/test'
+    import Datepicker from 'vuejs-datepicker'
+    import moment from 'moment'
 
     export default({
+        data: () => ({
+            startDate: moment().format(),
+            endDate: moment().format()
+        }),
         components: {
-            test
+            test,
+            Datepicker
         },
         created() {
-            console.log('ssa');
+            console.log(moment().startOf('week').isoWeekday(0));
         }
     })
 </script>
+<style scoped>
+    .vdp-datepicker {
+        display: inline-block!important;
+    }
+</style>
