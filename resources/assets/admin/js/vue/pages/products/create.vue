38b5315dae770e1ad5322cd96bc9f68095da8847
a_silivanov <andrey.silivanovv@gmail.com>
2018-04-03 16:50:35 +0300

    rename visible label

--- resources/assets/admin/js/vue/pages/products/create.vue
+++ resources/assets/admin/js/vue/pages/products/create.vue
@@ -122,7 +122,7 @@
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="visible">Visible
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="visible"> Add to the Shop Page
 
                 </label>
                 <div class="col-md-3 col-sm-3 col-xs-12">
