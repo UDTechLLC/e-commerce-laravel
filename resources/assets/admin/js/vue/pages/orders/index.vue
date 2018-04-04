3f65534ab37cfe80285441658b0b58e75ed1fa29
a_silivanov <andrey.silivanovv@gmail.com>
2018-04-04 17:09:30 +0300

    delete default key

--- resources/assets/admin/js/vue/pages/orders/index.vue
+++ resources/assets/admin/js/vue/pages/orders/index.vue
@@ -8,6 +8,7 @@
                     <label class="control-label col-md-3 col-sm-3 col-xs-12">Search field</label>
                     <div class="col-md-9 col-sm-9 col-xs-12">
                         <select class="select2_single form-control" v-model="searchField" tabindex="-1">
+                            <option ></option>
                             <option  v-for="item in searchFields">{{item}}</option>
                         </select>
                     </div>
@@ -54,7 +55,7 @@
             return {
                 states: [],
                 searchQuery: '',
-                searchField: 'id',
+                searchField: '',
                 searchFields: [
                   'id', 'email', 'name'
                 ],
