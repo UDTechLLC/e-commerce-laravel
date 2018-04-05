493bfd1e7bd606fe2f460578016e0bf820170934
a_silivanov <andrey.silivanovv@gmail.com>
2018-04-05 17:33:13 +0300

    added vue template

---
+++ resources/assets/admin/js/vue/pages/subscriptions/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>dsadsa33</h1>
+    </div>
+</template>
+<script type="text/babel">
+    export default ({
+        created() {
+            this.getSubscriptions();
+        },
+        methods: {
+            getSubscriptions() {
+                console.log('Get Content')
+            }
+        }
+    })
+</script>
--- resources/views/admin/subscriptions/index.blade.php
+++ resources/views/admin/subscriptions/index.blade.php
@@ -1,4 +1,4 @@
 @extends('admin.layouts.app')
 @section('content')
-    <h1>fsdfsdfsdf sdf</h1>
+    <subscriptions></subscriptions>
 @endsection
