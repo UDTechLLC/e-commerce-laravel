d766917bfb45776f0653ce6065b455770d9585fb
a_silivanov <andrey.silivanovv@gmail.com>
2018-04-06 09:35:19 +0300

    added vue template

--- resources/assets/admin/js/vue/pages/subscriptions/index.vue
+++ resources/assets/admin/js/vue/pages/subscriptions/index.vue
@@ -1,10 +1,33 @@
 <template>
     <div>
-        <h1>dsadsa33</h1>
+        <div class="container">
+            <h2>Orders {{ msg }}</h2>
+            <table class="table table-striped table-bordered bulk_action">
+                <!--<thead>
+                <tr>
+                    <th class="sorting_desc"
+                        v-for="key in columns">
+                        {{ key | capitalize }}
+                    </th>
+                </tr>
+                </thead>-->
+
+                <!--<tbody>
+                <tr v-for="(entry, index) in data">
+                    <td v-for="key in columns" v-html="entry[key]">
+
+                    </td>
+                </tr>
+                </tbody>-->
+            </table>
+        </div>
     </div>
 </template>
 <script type="text/babel">
     export default ({
+        data:() => ({
+           msg: "Hello"
+        }),
         created() {
             this.getSubscriptions();
         },
