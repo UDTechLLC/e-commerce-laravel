9a4b81ca50db4650532e3258cc58ff9077bf2ae6
a_silivanov <andrey.silivanovv@gmail.com>
2018-04-19 15:12:39 +0300

    top selers custo

--- app/Services/Statistic/ProductsStatisticService.php
+++ app/Services/Statistic/ProductsStatisticService.php
@@ -229,8 +229,8 @@ class ProductsStatisticService
     private function getTotalCustomPeriodStats(string $startDate, string $endDate): array
     {
         return \DB::select(\DB::raw('
-            select p.id, p.title, p.slug, res.count from products as p inner join (
-                select slug, sum(opres.count) as `count`
+            select p.id, p.title, p.slug, res.count, res.total from products as p inner join (
+                select slug, sum(opres.count) as `count`, sum(opres.product_price) as `total`
                     from products
                         left join (select op.* from orders o 
                             inner join order_product op on o.id = op.order_id and o.state = "PROCESSING") opres
