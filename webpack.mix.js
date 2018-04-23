2210bffb9a6e28a3612385a7e8b77025e923fb7e
a_silivanov <andrey.silivanovv@gmail.com>
2018-04-23 18:03:04 +0300

    minify style and script

--- webpack.mix.js
+++ webpack.mix.js
@@ -26,20 +26,27 @@ mix.options({
 
 
 mix.sass('resources/assets/admin/sass/app.scss', 'public/admin_panel/css')
-    .js('resources/assets/admin/js/app.js', 'public/admin_panel/js')
+    .js('resources/assets/admin/js/app.js', 'public/admin_panel/js').version()
     .copy('resources/assets/admin/images', 'public/admin_panel/images')
     .copy('resources/assets/admin/fonts', 'public/admin_panel/fonts')
 
     .js('resources/assets/js/app.js', 'public/web/js').version()
     .copy('resources/assets/js/chat.js', 'public/web/js')
-    .sass('resources/assets/sass/app.scss', 'public/web/css').version()
-    .sass('resources/assets/web/sass/base.sass', 'public/web/css').version()
+    //.sass('resources/assets/sass/app.scss', 'public/web/css').version()
+  //  .sass('resources/assets/web/sass/base.sass', 'public/web/css').version()
     .copy('resources/assets/web/css/', 'public/web/css')
+
     .copy('resources/assets/web/images', 'public/web/images')
     .copy('resources/assets/web/fonts', 'public/web/fonts')
     .copy('resources/assets/web/js', 'public/web/js')
     .copy('resources/assets/web/carouselengine', 'public/web/carouselengine')
-    .copy('resources/assets/web/slick', 'public/web/slick');
+    .copy('resources/assets/web/slick', 'public/web/slick')
+    .minify('public/web/css/base.css').version()
+    .minify('public/web/slick/slick.css').version()
+    .minify('public/web/slick/slick-theme.css').version()
+    .minify('public/web/js/modal.js').version()
+    .js('resources/assets/web/js/flipdown.js', 'public/web/js/flipdown.min.js').version()
+    .js('resources/assets/web/js/script.js', 'public/web/js/script.min.js').version();
 /*.js([
         `${path}jquery/dist/jquery.js`,
         `${path}bootstrap/dist/js/bootstrap.js`
