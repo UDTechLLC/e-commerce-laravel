4683c394bec6c89193924c9427bfbc62517fe777
a_silivanov <andrey.silivanovv@gmail.com>
2018-04-23 18:13:43 +0300

    minify style and script

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -38,13 +38,13 @@
     <!-- ////favicons-->
 
     @section('style')
-        {{--<link rel="stylesheet" href="{{ mix('web/css/app.css') }}">--}}
+        <link rel="stylesheet" href="{{ mix('web/css/app.css') }}">
 
-        <link rel="stylesheet" href="{{ mix('web/css/base.min.css') }}">
+        <link rel="stylesheet" href="{{ mix('web/css/base.css') }}">
         {{--<link rel="stylesheet" type="text/css" href="{{ asset('web/carouselengine/initcarousel-1.css') }}">--}}
         {{-- <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">--}}
-        <link rel="stylesheet" type="text/css" href="{{ asset('web/slick/slick.min.css') }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('web/slick/slick-theme.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('web/slick/slick.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('web/slick/slick-theme.css') }}">
         <!--[if lt IE 9]!!>
         <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
@@ -122,9 +122,9 @@
     <script type="text/javascript" src="{{ asset('web/slick/slick.min.js')}} "></script>
     <script type="text/javascript"
             src="https://cdnjs.cloudflare.com/ajax/libs/parallax.js/1.5.0/parallax.min.js"></script>
-    <script src="{{ asset('web/js/flipdown.min.js')}}"></script>
-    <script src="{{ asset('web/js/script.min.js') }}"></script>
-    <script src="{{ asset('web/js/modal.min.js')}}"></script>
+    <script src="{{ asset('web/js/flipdown.js')}}"></script>
+    <script src="{{ asset('web/js/script.js') }}"></script>
+    <script src="{{ asset('web/js/modal.js')}}"></script>
 
     <script>
         document.addEventListener('DOMContentLoaded', function () {
