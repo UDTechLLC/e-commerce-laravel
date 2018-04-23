d1c50a2443013494534c6e986681f8bbc3ea89c7
a_silivanov <andrey.silivanovv@gmail.com>
2018-04-23 16:17:45 +0300

    fixed

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\{
-    Order, OrderProduct, User
+    Order, OrderProduct
 };
 use App\Services\GoogleAnalyticsService;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
@@ -26,7 +25,7 @@ class HomeController extends Controller
         });
 
         $googleService = new GoogleAnalyticsService();
-        
+
         return view('admin.dashboard', [
             'totalOrders'   => $order->count(),
             'totalUsers'    =>
@@ -50,71 +49,5 @@ class HomeController extends Controller
             'browsers' => $googleService->getBrowserAnalytics(),
             'sources' => $googleService->getSourceAnalytics()
         ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
