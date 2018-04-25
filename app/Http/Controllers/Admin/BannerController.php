9be386abc984229da6fe186a321d9025aa7cfcd3
a.polikarpov <polik.palam@gmail.com>
2018-04-25 15:27:00 +0300

    Added store banner

--- app/Http/Controllers/Admin/BannerController.php
+++ app/Http/Controllers/Admin/BannerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Banner;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -15,8 +16,60 @@ class BannerController extends Controller
         return view('admin.banners.create');
     }
 
+    /**
+     * @param Request $request
+     *
+     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded
+     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\InvalidBase64Data
+     */
     public function store(Request $request)
     {
-        dd($request->all());
+        $banner = $this->createBanner($request->all());
+
+        $this->saveImageBase64($request->get('imageDesktop'), $banner, 'desktop');
+        $this->saveImageBase64($request->get('imageMobile'), $banner, 'mobile');
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return mixed
+     */
+    private function createBanner(array $data)
+    {
+        return Banner::create([
+            'title' => $data['title'],
+            'description' => $data['description'],
+            'link' => $data['link'],
+        ]);
+    }
+
+    /**
+     * @param string $base64Data
+     * @param Banner $banner
+     * @param string $collection
+     *
+     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded
+     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\InvalidBase64Data
+     */
+    private function saveImageBase64(string $base64Data, Banner $banner, string $collection)
+    {
+        $imageType = $this->getImageTypeFromBase64($base64Data);
+
+        $banner->addMediaFromBase64($base64Data)
+            ->usingFileName("{$banner->title}_{$collection}.{$imageType}")
+            ->toMediaCollection($collection);
+    }
+
+    /**
+     * @param string $data
+     *
+     * @return mixed
+     */
+    private function getImageTypeFromBase64(string $data)
+    {
+        $pos  = strpos($data, ';');
+
+        return explode('/', substr($data, 0, $pos))[1];
     }
 }
