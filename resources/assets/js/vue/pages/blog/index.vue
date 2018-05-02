a752af3a118ee3e7ff798c9971cfd5227d8e5dcd
a_silivanov <andrey.silivanovv@gmail.com>
2018-05-02 13:05:57 +0300

    change template

--- resources/assets/js/vue/pages/blog/index.vue
+++ resources/assets/js/vue/pages/blog/index.vue
@@ -1,36 +1,5 @@
 <template>
-    <main class="blog">
-        <div class="blog__heading-wrapper">
-            <div class="blog__heading">
-                <h1 class="blog__heading-title">
-                    Blog
-                </h1>
-            </div>
-        </div>
-        <div class="blog__navigation-wrapper">
-            <nav class="blog__navigation">
-                <ul>
-                    <li>
-                        <a class="tab-link" href="#">Exercises</a>
-                    </li>
-                    <li class="active">
-                        <a class="tab-link" href="#">Blog</a>
-                    </li>
-                    <li>
-                        <a class="tab-link" href="#">Vlog</a>
-                    </li>
-                    <li>
-                        <a class="tab-link" href="#">Workouts</a>
-                    </li>
-                    <li>
-                        <a class="tab-link" href="#">Recipes</a>
-                    </li>
-                    <li>
-                        <a class="tab-link" href="#">Education</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+    <div>
         <div class="blog__posts">
             <article class="blog__post" v-for="post in posts">
                 <a class="blog__post-top" :href="'/blog/'+ post.slug">
@@ -44,7 +13,7 @@
                     </h2>
                     <div class="blog__post-bottom-info">
                         <div class="blog__post-bottom-meta-date">
-                            <img class="blog__post-icon" src="web/images/blog/blog-icon.svg" />
+                            <img class="blog__post-icon" src="web/images/blog/blog-icon.svg"/>
                             <span class="blog__post-separator">|</span>
                             <p class="blog__post-bottom-meta-line">
                                 {{ post.posted_at }}
@@ -52,14 +21,14 @@
                         </div>
                         <span class="blog__post-separator">|</span>
                         <div class="blog__post-bottom-meta-view">
-                            <img class="blog__post-icon" src="web/images/blog/icon-white.svg" />
+                            <img class="blog__post-icon" src="web/images/blog/icon-white.svg"/>
                                 <span class="blog__post-text">
                                     {{ post.views }}
                                 </span>
                         </div>
                         <span class="blog__post-separator">|</span>
                         <div class="blog__post-bottom-meta-likes">
-                            <img class="blog__post-icon" src="web/images/blog/icon-heart-white.svg" />
+                            <img class="blog__post-icon" src="web/images/blog/icon-heart-white.svg"/>
                                 <span class="blog__post-text">
                                     0
                                 </span>
@@ -71,16 +40,16 @@
         <div class="blog__load-more" @click="getPosts" v-if="url">
             Load more posts
         </div>
-    </main>
+    </div>
 </template>
 <script type="text/babel">
     export default ({
-       data: () => ({
-           posts: [],
-           url: `/blog/all?page=1`
-       }),
+        data: () => ({
+            posts: [],
+            url: `/blog/all?page=1`
+        }),
         created() {
-          this.getPosts()
+            this.getPosts()
         },
         methods: {
             getPosts() {
@@ -90,7 +59,6 @@
                             this.url = response.data.links.next;
                         },
                         error => console.log('error')
-
                 )
             }
         }
--- resources/views/web/blog/index.blade.php
+++ resources/views/web/blog/index.blade.php
@@ -2,7 +2,41 @@
 @section('style')
     @parent
     <link rel="stylesheet" href="{{asset('web/css/blog.css')}}">
-    @endsection
+@endsection
 @section('content')
-    <blog></blog>
+    <main class="blog">
+        <div class="blog__heading-wrapper">
+            <div class="blog__heading">
+                <h1 class="blog__heading-title">
+                    Blog
+                </h1>
+            </div>
+        </div>
+        <div class="blog__navigation-wrapper">
+            <nav class="blog__navigation">
+                <ul>
+
+                    <li>
+                        <a class="tab-link" href="#">Exercises</a>
+                    </li>
+                    <li class="active">
+                        <a class="tab-link" href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a class="tab-link" href="#">Vlog</a>
+                    </li>
+                    <li>
+                        <a class="tab-link" href="#">Workouts</a>
+                    </li>
+                    <li>
+                        <a class="tab-link" href="#">Recipes</a>
+                    </li>
+                    <li>
+                        <a class="tab-link" href="#">Education</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <blog></blog>
+    </main>
 @endsection
