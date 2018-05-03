383f72f00cf05f6046c830571e555e3c6990f62a
a_silivanov <andrey.silivanovv@gmail.com>
2018-05-03 12:55:28 +0300

    banner fixed

--- resources/views/admin/banners/partials/template.blade.php
+++ resources/views/admin/banners/partials/template.blade.php
@@ -1,3 +1,6 @@
-<a href="{{ $banner->link }}">
-    <img src="{{$banner->getFirstMediaUrl('desktop')}}">
-</a>
+<div class="blog-post__content-banner">
+    <a href="{{ $banner->link }}">
+    <img class="desktop-banner-image" src="{{ $banner->getFirstMediaUrl('desktop') }}" />
+    <img class="mobile-banner-image" src="{{ $banner->getFirstMediaUrl('mobile') }}" />
+    </a>
+</div>
