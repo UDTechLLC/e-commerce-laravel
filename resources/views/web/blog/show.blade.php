2dcff6158a782547461a622f88d4b3e0ab384685
a_silivanov <andrey.silivanovv@gmail.com>
2018-05-03 11:41:10 +0300

    share button

--- resources/views/web/blog/show.blade.php
+++ resources/views/web/blog/show.blade.php
@@ -11,6 +11,20 @@
     <link rel="stylesheet" href="{{asset('web/css/blog.css')}}">
     <link rel="stylesheet" href="{{asset('web/css/blog_post.css')}}">
 @endsection
+@section('body-script')
+    @parent
+    <script>
+        window.fbAsyncInit = function () {
+            FB.init({
+                appId: '1580805958670684',
+                autoLogAppEvents: true,
+                xfbml: true,
+                version: 'v2.10'
+            });
+            FB.AppEvents.logPageView();
+        };
+    </script>
+@endsection
 @section('content')
     <main class="blog-post">
         <article class="blog-post__content">
@@ -24,7 +38,7 @@
             <div class="blog-post__content-social">
                 <div class="blog-post__content-viewlike">
                     <div class="blog-post__content-view">
-                        <img src="{{asset('web/images/blog/view-desk-icon.svg')}}" />
+                        <img src="{{asset('web/images/blog/view-desk-icon.svg')}}"/>
                             <span class="blog-post__content-view-counter">
                                 {{ $post->view_count }}
                             </span>
@@ -43,7 +57,8 @@
                         </span>
                     <ul class="blog-post__content-share-list" style="">
                         <li class="blog-post__content-share-item">
-                            <a class="facebook-share blog-post__content-share-item-link">
+                            <a class="facebook-share blog-post__content-share-item-link"
+                               onclick="facebookShare(event);">
                                 <i class="fab fa-facebook-f"></i>
                             </a>
                         </li>
@@ -53,15 +68,26 @@
                             </a>
                         </li>
                         <li class="blog-post__content-share-item">
-                            <a class="pinterest-share blog-post__content-share-item-link">
+                            <a class="pinterest-share blog-post__content-share-item-link"
+                               target="_blank" href="http://pinterest.com/pin/create/button/?url={{ Request::url() }}&media={{asset($post->previewImage)}}&description={{ $post->title }}">
                                 <i class="fab fa-pinterest-p"></i>
                             </a>
                         </li>
                         <li class="blog-post__content-share-item">
-                            <a class="email-share blog-post__content-share-item-link">
+                            <a class="email-share blog-post__content-share-item-link"
+                               href="mailto:?subject={{$post->title}}&amp;body={{ Request::url() }}"
+                               title="Share by Email">
                                 <i class="far fa-envelope"></i>
                             </a>
                         </li>
+                        {{--<li>
+                            <a target="_blank" href="http://pinterest.com/pin/create/button/?url={{ Request::url() }}&media={{asset($post->previewImage)}}&description={{ $post->title }}" class="pin-it-button" count-layout="horizontal">
+                                <img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" />
+                            </a>
+                        </li>--}}
+                       {{-- <li>
+                            <a data-pin-do="buttonBookmark" data-pin-lang="en" href="https://www.pinterest.com/pin/create/button/"></a>
+                        </li>--}}
                     </ul>
                 </div>
             </div>
@@ -91,7 +117,8 @@
                             </div>
                             <span class="blog__post-separator">|</span>
                             <div class="blog__post-bottom-meta-likes">
-                                <img class="blog__post-icon" src="{{ asset('/web/images/blog/icon-heart-white.svg') }}"/>
+                                <img class="blog__post-icon"
+                                     src="{{ asset('/web/images/blog/icon-heart-white.svg') }}"/>
                                 <span class="blog__post-text">
                                     {{ $item->likes_count }}+
                                 </span>
@@ -102,4 +129,32 @@
             @endforeach
         </div>
     </main>
+@endsection
+@section('scripts')
+    @parent
+  {{--  <script async defer src="//assets.pinterest.com/js/pinit.js"></script>--}}
+    <script>
+
+        $(function () {
+            /*
+             * Twitter
+             * */
+            var twitterPath = 'https://twitter.com/share?';
+            // var url = 'https%3A%2F%2Fdev.twitter.com%2Fweb%2Ftweet-button';
+            var url = window.location.href;
+            var text = '{{$post->title}}';
+            $('.twitter-share').attr('href', `${twitterPath}url=${url}&text=${text}`);
+        });
+        /*
+         * facebook
+         * */
+        function facebookShare(e) {
+            e.preventDefault();
+            FB.ui({
+                method: 'share',
+                href: window.location.href,
+            }, function (response) {
+            });
+        }
+    </script>
 @endsection
