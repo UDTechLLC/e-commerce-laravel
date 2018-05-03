304dbfaf0bd601a095001e74613ef5116282b8f9
a.polikarpov <polik.palam@gmail.com>
2018-05-03 11:27:29 +0300

    Added shipping model, change get shipping logic

--- app/helpers.php
+++ app/helpers.php
@@ -4,7 +4,19 @@ declare(strict_types = 1);
 
 function getShippingCost(string $country)
 {
-    return $country === 'United States'
-        ? number_format(6.99, 2)
-        : number_format(17.99, 2);
+//    return $country === 'United States'
+//        ? number_format(6.99, 2)
+//        : number_format(17.99, 2);
+    $shipping = App\Models\Shipping::where('country', $country)->first();
+    $other = App\Models\Shipping::where('country', 'other')->first();
+    $freeCost = number_format(0, 2);
+
+    return
+        null !== $shipping
+            ? $shipping->isFree
+                ? $freeCost
+                : $shipping->cost
+            : $other->isFree
+                ? $freeCost
+                : $other->cost;
 }
