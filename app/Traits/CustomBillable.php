5d51a5fb359022e0a116c911b38c3634e12ffe08
a.polikarpov <polik.palam@gmail.com>
2018-05-04 16:38:03 +0300

    Added custom billable trait

---
+++ app/Traits/CustomBillable.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Traits;
+
+use Laravel\Cashier\Billable;
+
+/**
+ * Trait CustomBillable
+ * @package App\Traits
+ */
+trait CustomBillable
+{
+    use Billable;
+}
