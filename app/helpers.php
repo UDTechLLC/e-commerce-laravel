1787238052a252af6bab11cbe9685ba9796be28a
a.polikarpov <polik.palam@gmail.com>
2018-05-04 17:45:30 +0300

    Fix calculatinf shipping

--- app/helpers.php
+++ app/helpers.php
@@ -9,14 +9,13 @@ function getShippingCost(string $country)
     $other = App\Models\Shipping::where('country', 'other')->first();
     $freeCost = number_format(0, 2);
 
-    return
-        null !== $shipping
-            ? $shipping->isFree
-                ? $freeCost
-                : $shipping->cost
-            : $other->isFree
-                ? $freeCost
-                : $other->cost;
+    if (null !== $shipping) {
+        return $shipping->isFree ? $freeCost : $shipping->cost;
+    } elseif (null !== $other) {
+        return $other->isFree ? $freeCost : $other->cost;
+    }
+
+    return $freeCost;
 }
 
 function freeShipping()
