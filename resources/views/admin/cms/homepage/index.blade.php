c3740d8cd07c6efb0ad7a8824c8d7155a4d7c863
a_silivanov <andrey.silivanovv@gmail.com>
2018-05-07 16:16:54 +0300

    fixed title

--- resources/views/admin/cms/homepage/index.blade.php
+++ resources/views/admin/cms/homepage/index.blade.php
@@ -6,14 +6,7 @@
     <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
             <div class="x_title">
-                <h2>Categories list</h2>
-                <div class="nav navbar-right panel_toolbox">
-                    <a href="{{ route('admin.categories.create') }}">
-                        <button class="btn btn-sm btn-success btn-block">
-                            <i class="fa fa-plus"></i> Create Category
-                        </button>
-                    </a>
-                </div>
+                <h2>Banners list</h2>
                 <div class="clearfix"></div>
             </div>
             <div class="x_content">
