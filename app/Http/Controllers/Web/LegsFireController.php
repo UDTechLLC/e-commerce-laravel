7590b9823fc759b4ee1cb6a9937d762584d31053
a.polikarpov <polik.palam@gmail.com>
2018-05-23 15:04:45 +0300

    Refatc code

--- app/Http/Controllers/Web/LegsFireController.php
+++ app/Http/Controllers/Web/LegsFireController.php
@@ -16,47 +16,45 @@ class LegsFireController extends Controller
     {
         return view('web.legs.legs_first');
     }
+
     /**
      * Get legs on fire page week 2.
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-
     public function legsSecond()
     {
         return view('web.legs.legs_second');
     }
+
     /**
-     * Get legs on fire page week 3.
-     *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-
     public function legsThird()
     {
         return view('web.legs.legs_third');
     }
 
     /**
- * Get legs on fire page week 4.
- *
- * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
- */
-
+     * Get legs on fire page week 4.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function legsFourth()
     {
         return view('web.legs.legs_fourth');
     }
+
     /**
      * Get legs on fire page week 5.
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-
     public function legsFifth()
     {
         return view('web.legs.legs_fifth');
     }
+
     /**
      * Get total body challenge page step 1 to 10.
      *
@@ -66,6 +64,7 @@ class LegsFireController extends Controller
     {
         return view('web.legs.total_challenge');
     }
+
     /**
      * Get total body challenge page step 11 to 20.
      *
@@ -75,6 +74,7 @@ class LegsFireController extends Controller
     {
         return view('web.legs.total_challenge_second');
     }
+
     /**
      * Get total body challenge page step 21 to 30.
      *
@@ -84,5 +84,4 @@ class LegsFireController extends Controller
     {
         return view('web.legs.total_challenge_third');
     }
-
 }
