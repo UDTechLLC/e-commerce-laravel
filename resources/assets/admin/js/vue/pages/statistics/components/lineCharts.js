34b24a5be2c3d05b1d15058487393c52564ccf94
a_silivanov <andrey.silivanovv@gmail.com>
2018-05-30 17:45:32 +0300

    fixed

--- resources/assets/admin/js/vue/pages/statistics/components/lineCharts.js
+++ resources/assets/admin/js/vue/pages/statistics/components/lineCharts.js
@@ -12,7 +12,6 @@ export default ({
   }),
   props: ['chartData', 'options'],
   mounted () {
-    console.log(this.chartData);
     this.gradient = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450);
     this.gradient2 = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450);
     this.gradient3 = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450);
@@ -26,12 +25,12 @@ export default ({
     this.gradient2.addColorStop(0.5, 'rgba(0, 231, 255, 0.5)');
     this.gradient2.addColorStop(1, 'rgba(0, 231, 255, 0)');
 
-    this.gradient3.addColorStop(0, 'rgba(145,232,66, 1)');
-    this.gradient3.addColorStop(0.5, 'rgba(145,232,66, 0.5)');
-    this.gradient3.addColorStop(1, 'rgba(145,232,66, 0)');
+    this.gradient3.addColorStop(0, 'rgba(145, 232, 66, 1)');
+    this.gradient3.addColorStop(0.5, 'rgba(145, 232, 66, 0.5)');
+    this.gradient3.addColorStop(1, 'rgba(145, 232, 66, 0)');
 
     this.gradient4.addColorStop(0, 'rgba(162, 111, 252, 1)');
-    this.gradient4.addColorStop(0.5, 'rgba(162, 111, 252, 5)');
+    this.gradient4.addColorStop(0.5, 'rgba(162, 111, 252, 0.5)');
     this.gradient4.addColorStop(1, 'rgba(162, 111, 252, 0)');
 
 
@@ -43,7 +42,7 @@ export default ({
           borderColor: 'rgba(255, 0, 0, 1)',
           pointBackgroundColor: 'white',
           borderWidth: 1,
-          pointBorderColor: '#eeeeee',
+          pointBorderColor: '#000000',
           backgroundColor: this.gradient,
           data: this.chartData.datasets[0].data
         },
@@ -52,7 +51,7 @@ export default ({
           borderColor: 'rgba(0, 231, 255, 1)',
           pointBackgroundColor: 'white',
           borderWidth: 1,
-          pointBorderColor: '#eeeeee',
+          pointBorderColor: '#000000',
           backgroundColor: this.gradient2,
           data: this.chartData.datasets[1].data
         },
@@ -87,9 +86,7 @@ export default ({
         displayColors: false,
         intersect: false
       },
-      legend: {
-        display: false
-      },
-      responsive: true, maintainAspectRatio: false})
+      responsive: true,
+      maintainAspectRatio: false})
   }
 })
